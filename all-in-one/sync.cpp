84d48eb223d7b3243b1b31062994b3648f05e04c
Jonathan Dan <danjjl@hotmail.com>
2013-03-10 23:51:25 +0100

    Ajouté ligne confirmant qu'on est pret à recevoir les utilisateurs && remplacé espaces par \t

--- all-in-one/sync.cpp
+++ all-in-one/sync.cpp
@@ -11,49 +11,50 @@ void sync(){
 	lcd.print("...");
 
 	//Read Values from memory
-        while(address < 1000 && value != 0) {
-          if(Serial.available()) {
-            Serial.read();
-            value = EEPROM.read(address);
-            EEPROM.write(address, 0);
-            Serial.print(value);
-            delay(100);
-            address ++;
-          }
-        }
-        
-        lcd.clear();
+	while(address < 1000 && value != 0) {
+		if(Serial.available()) {
+			Serial.read();
+			value = EEPROM.read(address);
+			EEPROM.write(address, 0);
+			Serial.print(value);
+			delay(100);
+			address ++;
+		}
+	}
+	
+	lcd.clear();
 	lcd.setCursor(0,0);
 	lcd.print("Sent mesures");
 	lcd.setCursor(12,1);
 	lcd.print("...");
-        
-        value = 1;
-        address = 999;
-        while(address >= 0 && value != 0){
-          value = EEPROM.read(address);
-          EEPROM.write(address, 0);
-          address --;
-        }
-        
-        lcd.clear();
-        lcd.setCursor(0,0);
+	
+	value = 1;
+	address = 999;
+	while(address >= 0 && value != 0){
+		value = EEPROM.read(address);
+		EEPROM.write(address, 0);
+		address --;
+	}
+	
+	lcd.clear();
+	lcd.setCursor(0,0);
 	lcd.print("Receive users");
 	lcd.setCursor(12,1);
 	lcd.print("...");
-        
-        value = 1;
-        address = 999;
-        while(address >= 0 && value != 0){
-          if(Serial.available()) {
-            value = Serial.read();
-            EEPROM.write(address, value);
-            Serial.print('0');
-            delay(100);
-            address --;
-          }
-        }
-        Serial.print('F');
-        
+
+	Serial.print('r');//Print ready
+	value = 1;
+	address = 999;
+	while(address >= 0 && value != 0){
+		if(Serial.available()) {
+			value = Serial.read();
+			EEPROM.write(address, value);
+			Serial.print('0');
+			delay(100);
+			address --;
+		}
+	}
+	Serial.print('F');
+	
 //TODO write Serial.read() and and erase users from db write users to end of eeprom
 }
